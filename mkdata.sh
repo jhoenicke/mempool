99e4c382a4f0c1532b29d4ed0c19749ad6110677
Jochen Hoenicke <hoenicke@gmail.com>
2017-05-12 01:00:42 +0200

    Write to files atomically.
    Write to temporary file and rename atomically to prevent glitches, when
    someone opens the page at the wrong moment.

--- mkdata.sh
+++ mkdata.sh
@@ -5,31 +5,39 @@ DESTDIR=/dev/shm/mempool
 mkdir -p $DESTDIR
 
 createfile() {
-  NAME=$1
-  MINUTES=$2
-  (echo 'call(['; tail -$MINUTES $MEMPOOL; echo '])') > $DESTDIR/$NAME.js
+  (echo 'call(['; eval "$CMD"; echo '])') > $DESTDIR/$NAME.js.new
+  mv $DESTDIR/$NAME.js.new $DESTDIR/$NAME.js
 }
 
-createfilei() {
+createfile_unfiltered() {
+  NAME=$1
+  MINUTES=$2
+  CMD="tail -$MINUTES $MEMPOOL"
+  createfile
+}
+
+createfile_filtered() {
   NAME=$1
   MINUTES=$2
   IVAL=$3
-  (echo 'call(['; tail -$MINUTES $MEMPOOL | sed -n '1~'$IVAL'p'; echo '])') > $DESTDIR/$NAME.js
+  CMD="tail -$MINUTES $MEMPOOL | sed -n '1~${IVAL}p'"
+  createfile
 }
 
-createfileall() {
+createfile_all() {
   NAME=all
   IVAL=$1
-  (echo 'call(['; sed -n '1~'$IVAL'p' < $MEMPOOL; echo '])') > $DESTDIR/$NAME.js
+  CMD="sed -n '1~${IVAL}p' < $MEMPOOL"
+  createfile
 }
 
-createfile 2h 120
-createfile 8h 480
-createfile 24h 1440
-createfilei 2d 2880 2
-createfilei 4d 5760 4
-createfilei 1w 10080 7
-createfilei 2w 20160 14
-createfilei 30d 43200 30
-createfilei 3m 131040 90
-createfileall 120
+createfile_unfiltered 2h 120
+createfile_unfiltered 8h 480
+createfile_unfiltered 24h 1440
+createfile_filtered 2d 2880 2
+createfile_filtered 4d 5760 4
+createfile_filtered 1w 10080 7
+createfile_filtered 2w 20160 14
+createfile_filtered 30d 43200 30
+createfile_filtered 3m 131040 90
+createfile_all 120
